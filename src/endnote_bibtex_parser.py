a1258aa317a34d122fb543e21f669353985f4592
Richard <42204099+rikroe@users.noreply.github.com>
2024-10-20 22:42:10 +0200

    fix duplicate IDs

--- src/endnote_bibtex_parser.py
+++ src/endnote_bibtex_parser.py
@@ -3,11 +3,13 @@ from pathlib import Path
 from typing import Optional
 
 import bibtexparser
+import numpy as np
+import pandas as pd
 import slugify
 from bibtexparser import customization
 
 
-async def parse(input: Optional[str] = None, output: Optional[str] = None, remove_notes: bool = True):
+async def parse(input: Optional[str] = None, output: Optional[str] = None, remove_notes: bool = True) -> str | None:
     """Parse and homogenize a BibTex from Endnote."""
 
     input_path = Path(input or "")
@@ -26,14 +28,35 @@ async def parse(input: Optional[str] = None, output: Optional[str] = None, remov
         entry["ID"] = slugify.slugify(id, separator="_")
         entry["key"] = slugify.slugify(id)
 
-        if remove_notes is True and "note" in entry:
-            entry.pop("note")
+        if remove_notes is True:
+            entry.pop("note", None)
+            entry.pop("keywords", None)
 
         entry = customization.homogenize_latex_encoding(entry)
 
+    bib_database_df = pd.DataFrame(bib_database.entries)
+    bib_database_df["key"] = bib_database_df["key"].astype(str)
+    bib_database_df["doi"] = bib_database_df["doi"].astype(str)
+
+    duplicate_keys = bib_database_df["key"].duplicated(keep=False)
+
+    bib_database_df["key"] = np.where(
+        duplicate_keys,
+        bib_database_df.apply(lambda x: "-".join([c for c in [x["key"], x["doi"]] if c]), axis=1),
+        bib_database_df["key"],
+    )
+    bib_database_df["ID"] = bib_database_df["key"].str.replace("-", "_")
+
+    bib_database.entries = [{k: v for k, v in x.items() if v == v} for x in bib_database_df.to_dict(orient="records")]
+
     bib_database.add_missing_from_crossref()
 
+    if not output:
+        return bibtexparser.dumps(bib_database)
+
     await asyncio.to_thread(output_path.write_text, bibtexparser.dumps(bib_database))
+
+    return None
 
 
 if __name__ == "__main__":
